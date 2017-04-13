40ed6a0bf1b019c82020a48d00f0f4f4cf2bf956
Jakob <jakob.ruhl@gmail.com>
2017-04-13 10:35:51 +0200

    fixed comments for real now

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // Static comments
 (function ($) {
-    
+
   $('#contactForm').submit(function (event) {
       console.log("action")
       event.preventDefault();
     var form = this;
+
+    showAlert("sending to server")
 
     $.ajax({
       type: $(this).attr('method'),
@@ -12,13 +14,24 @@
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        console.log("yey");
+        $('#comment-form-submit').html('Submitted');
+        $('#contactForm .js-notice').removeClass('notice--danger').addClass('notice--success');
+        showAlert('<strong>Thanks for your comment!</strong> It will show on the site once it has been approved.');
       },
       error: function (err) {
         console.log(err);
+        $('#comment-form-submit').html('Submit Comment');
+        $('.post__comments-form .js-notice').removeClass('notice--success').addClass('notice--danger');
+        showAlert('<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again.');
+        $(form).removeClass('disabled');
       }
     });
 
     return false;
   });
+
+  function showAlert(message) {
+    $('#contactForm .js-notice').removeClass('hidden');
+    $('#contactForm .js-notice-text').html(message);
+  }
 })(jQuery);
